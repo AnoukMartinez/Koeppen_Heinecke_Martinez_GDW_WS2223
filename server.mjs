14e55b1cd2c2e4353e68508d1cc121fad6a2486d
Martinez <anoukmartinez@outlook.de>
2022-12-20 16:39:05 +0100

    Autorisierung

--- server.mjs
+++ server.mjs
@@ -8,8 +8,8 @@ const app = express();
 
 class Song {
     constructor(artist, name, popularity, spotify_id){
-        this.name = name,
         this.artist = artist,
+        this.name = name,
         this.popularity = popularity
         // this.spotify_id = spotify_id
     }
@@ -31,22 +31,28 @@ var token = "";
 var timeStamp = 0; 
 var LIMIT = 10;
 
+const admin = {username:"admin",password:"secret"};
+
 app.use(express.json());
 
-// MAKE NEW EVENT (body requires: name)
+// MAKE NEW EVENT (body requires: password, username, name)
 app.post('/events', (req, res) => {
-    let newParty = new Party(req.body.name);
-    events.push(newParty);
-    res.status(201).send("OK");
-})
+    if(authorizer(req.body.username, req.body.password)){
+        let newParty = new Party(req.body.name);
+        events.push(newParty);
+        res.status(201).send("OK");
+    } else {
+        res.status(405).send("You don't seem to be authorized for this action.");
+    };
+});
 
 // DISPLAY ALL EVENTS
 app.get('/events', (req, res) => {
     let allEventNames = [];
-    events.forEach(a => allEventNames.push({name: a.name, isActive: a.isActive}))
+    events.forEach(a => allEventNames.push({name: a.name, isActive: a.isActive}));
     res.json(allEventNames);
     res.status(200);
-})
+});
 
 // DISPLAY EVENT INFO
 app.get('/events/:eventId', (req, res) => {
@@ -55,15 +61,15 @@ app.get('/events/:eventId', (req, res) => {
         res.status(404).send("Event Does Not Exist...");
     } else {
         res.send(events[eventId]);
-    }
-})
+    };
+});
 
 // SEARCH A SONG (query: localhost:xxxx/songs/irgendeinname)
 app.get('/songs/:name', async (req, res) => {
-    const track = req.params.name
+    const track = req.params.name;
     const result = await searchTrack(track);
     res.json(result);
-})
+});
 
 // VOTE FOR EXISTING SONG (body requires: eventId (starts from 0), songId (starts from 0))
 app.put('/songs', (req, res) => {
@@ -81,26 +87,61 @@ app.put('/songs', (req, res) => {
         events[eventId].voting = events[eventId].voting.sort(({popularity : a}, {popularity : b}) => b - a);
         res.send(`Successfully voted for ${events[eventId].voting[songId].name}
                   by ${events[eventId].voting[songId].artist}!`);
-    }
-})
+    };
+});
 
 // ADD NEW SONG (body requries: eventId, artist, title)
 app.post('/songs', (req, res) => {
     let eventId = req.body.eventId;
     if(eventId < 0 || events.length <= eventId) {
         res.status(404).send("Event Does Not Exist...");
-    }
+    };
     let artist = req.body.artist;
     let title = req.body.title;
     let newSong = new Song(artist, title, 1);
     events[eventId].voting.push(newSong);
-    res.status(201).send(`Successfully added ${title} by ${artist}!`)
-})
+    res.status(201).send(`Successfully added ${title} by ${artist}!`);
+});
+
+// DELETES SONG FROM VOTING AND ADDS IT TO TRACKLIST (body requires: username, password, eventId, songId)
+app.put('/delete/songs', (req, res) => {
+    let eventId = req.body.eventId;
+    let songId = req.body.songId;
+
+    if(authorizer(req.body.username, req.body.password)){
+        if(songId > events[eventId].voting.length){
+            res.status(404).send("This song or event does not exist.");
+        } else {
+            let newSong = new Song(events[eventId].voting[songId].artist, 
+                                    events[eventId].voting[songId].name,
+                                    events[eventId].voting[songId].popularity)
+            events[eventId].voting.splice(songId, 1);
+            events[eventId].tracklist.push(newSong);
+            res.status(200).send(`Successfully deleted ${newSong.name} by ${newSong.artist}!`)
+        }
+    } else {
+        res.status(405).send("You don't seem to be authorized for this action.")
+    };
+});
+
+// CHANGES EVENT TO INACTIVE (body requires: username, password, eventId)
+app.put('/edit', (req, res) => {
+    let eventId = req.body.eventId;
+    if(authorizer(req.body.username, req.body.password)){
+        if(eventId > events.length){
+            res.status(404).send("This event does not exist.");
+        } else {
+            events[eventId].isActive = false;
+        }
+    } else {
+        res.status(405).send("You don't seem to be authorized for this action.");
+    };
+});
 
 // SERVER
 app.listen(3000, () => {
     // initTest();
-    console.log('Schau mal auf localhost:3000/')
+    console.log('Schau mal auf localhost:3000/');
 }); 
 
 function initTest(){
@@ -130,7 +171,7 @@ function fetchToken(){
             reject("Looks like something went wrong.");
         }
     })
-    })
+    });
     return promise;
 };
 
@@ -169,5 +210,11 @@ async function searchTrack(track){
                 };
                 resolve(allResults);
     })
-})
+});
 };
+
+function authorizer(username, password){
+    if((username == admin.username) && (password == admin.password)){
+        return true;
+    } else return false;
+}
